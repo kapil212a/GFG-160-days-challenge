a58b77fe742761286cd5483058f9483cfdb4ae1c
kapil212a <amangupta74086@gmail.com>
2025-06-24 16:37:17 +0530

    change

--- gfgString.java
+++ gfgString.java
@@ -26,5 +26,6 @@ public class gfgString {
     public static void main(String args[]){
         String s  = " -123";
         System.out.println(myAtoi(s));
+        System.err.println(s);
     }
 }
