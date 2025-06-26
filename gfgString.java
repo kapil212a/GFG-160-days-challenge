5a061997ba3eca93a21f32a19255650bedf417e4
kapil212a <amangupta74086@gmail.com>
2025-06-26 19:13:54 +0530

    change

--- gfgString.java
+++ gfgString.java
@@ -22,10 +22,57 @@ public class gfgString {
         }
         return res * sign;
     }
+
+    
+    static String removeZero(String s){
+        int first = s.indexOf("1");
+        return (first == -1) ? "0" : s.substring(first);
+    }
+    public static String addBinary(String s1, String s2) {
+        
+        s1 = removeZero(s1);
+        s2 = removeZero(s2);
+        
+        int n = s1.length();    
+        int m = s2.length();
+        
+        if(n<m){
+            return addBinary(s2,s1);
+        }
+        
+        int j = m-1;
+        int carry = 0;
+        StringBuilder result = new StringBuilder(); 
+        
+        for(int i = n-1;i>=0;i--){
+            int str1 = s1.charAt(i) - '0';
+            int sum = str1 + carry;
+            
+            if(j>=0){
+                int str2 = s2.charAt(j) - '0';
+                sum +=str2;
+                j--;
+            }
+            
+            int str3 = sum%2;
+            carry = sum/2;
+            
+            result.append((char)(str3 + '0'));
+        }
+        
+        if(carry>0){
+            result.append('1');
+        }
+        return result.reverse().toString();
+    }
     
     public static void main(String args[]){
-        String s  = " -123";
-        System.out.println(myAtoi(s));
-        System.err.println(s);
+        // String s  = " -123";
+        // System.out.println(myAtoi(s));
+        // System.err.println(s);
+
+        String s1 = "1101";
+        String s2 = "111";
+        System.out.println(addBinary(s1,s2));
     }
 }
