92267dc3b30b8cdc3fcd26d8cde2a9a417210bd6
kapil212a <amangupta74086@gmail.com>
2025-07-05 15:37:32 +0530

    change

--- Searching.java
+++ Searching.java
@@ -30,13 +30,40 @@ public class Searching {
         return arr[start];
         
     }
+    public static int searchInSortedAndRot(int arr[],int key){   ////o(nlogn)
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid] == key){
+                return mid;
+            }
+            if(arr[mid]>=arr[start]){
+                if(key>=arr[start] && key<arr[mid]){
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }else{
+                if(key>arr[mid] && key<=arr[end]){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }
+        }return -1;
+    }
 
     public static void main(String[] args) {
         // int arr[] = {1, 1, 2, 2, 2, 2, 3};
         // int target = 2;
         // System.out.println(countFreq(arr, target));
 
-        int arr[] = {5, 6, 1, 2, 3, 4};
-        System.out.println(findMin(arr));
+        // int arr[] = {5, 6, 1, 2, 3, 4};
+        // System.out.println(findMin(arr));
+
+        int[] arr1 = {5, 6, 7, 8, 9, 10, 1, 2, 3};
+        int key1 = 3;
+        System.out.println(searchInSortedAndRot(arr1, key1));
     }
 }
