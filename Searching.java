fa2ddbee272d656f67a1b8fe382687f6d200005e
kapil212a <amangupta74086@gmail.com>
2025-07-05 14:22:06 +0530

    change

--- Searching.java
+++ Searching.java
@@ -1,3 +1,17 @@
 public class Searching {
-    
+    static int countFreq(int[] arr, int target) {
+        
+        int count =0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == target){
+                count++;
+            }
+        }return count;
+    }
+
+    void main(String[] args) {
+        int arr[] = {1, 1, 2, 2, 2, 2, 3};
+        int target = 2;
+        System.out.println(countFreq(arr, target));
+    }
 }
