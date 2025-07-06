8c8eede45788da09a3f7fe572f3c50eb90887f13
kapil212a <amangupta74086@gmail.com>
2025-07-06 19:12:20 +0530

    change

--- Searching.java
+++ Searching.java
@@ -66,6 +66,34 @@ public class Searching {
         
     }
 
+     public static int peakElement(int[] arr) {
+        
+        int n = arr.length;
+        if(n==1){
+            return 0;
+        }
+        if(arr[0]>arr[1]){
+            return 0;
+        }
+        if(arr[n-1]>arr[n-2]){
+            return n-1;
+        }
+        int start = 1;
+        int end = n-2;
+        while(start<=end){
+            int mid  = (start+end)/2;
+            if(arr[mid] >arr[mid-1] && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            if(arr[mid]<arr[mid+1]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }return -1;
+    }
+
     public static void main(String[] args) {
         // int arr[] = {1, 1, 2, 2, 2, 2, 3};
         // int target = 2;
@@ -78,8 +106,12 @@ public class Searching {
         // int key1 = 3;
         // System.out.println(searchInSortedAndRot(arr1, key1));
 
-        int arr [] = {5, 6, 7, 8, 9, 10, 1, 2, 3};
-        int key = 3;
-        System.out.println(search(arr, key));
+        // int arr [] = {5, 6, 7, 8, 9, 10, 1, 2, 3};
+        // int key = 3;
+        // System.out.println(search(arr, key));
+
+        int arr[] = {1, 2, 4, 5, 7, 8, 3};
+        System.out.println(peakElement(arr));
+
     }
 }
