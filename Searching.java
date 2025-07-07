b85d9453761931d62cef2deb7e9978ed5afa2f26
kapil212a <amangupta74086@gmail.com>
2025-07-07 14:02:31 +0530

    change

--- Searching.java
+++ Searching.java
@@ -143,7 +143,40 @@ public class Searching {
             }
         }
         return res;
+    }
+    public static boolean checking(int arr[],int k,int max){
+        int count =1;
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            if(sum+arr[i]>max){
+                count++;
+                sum = arr[i];
+            }
+            else{
+                sum+=arr[i];
+            }
+        }
+        return (count<=k);
+    }
+    public static int findPages(int[] arr, int k) {
+       
+        if(k>arr.length){
+            return -1;
+        }
+        int low = Arrays.stream(arr).max().getAsInt();
+        int hi = Arrays.stream(arr).sum();
         
+        int res = -1;
+        while(low<=hi){
+            int mid = (hi+low)/2;
+            if(checking(arr,k,mid)){
+                res = mid;
+                hi = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
@@ -170,9 +203,12 @@ public class Searching {
         // int k = 5;
         // System.out.println(kthElement(a, b, k));
         
-        int arr[] = {1, 2, 4, 8, 9};
-        int k = 3;
-        System.out.println(aggressiveCows(arr, k));
+        // int arr[] = {1, 2, 4, 8, 9};
+        // int k = 3;
+        // System.out.println(aggressiveCows(arr, k));
 
+        int arr[] = {12, 34, 67, 90};
+        int k = 2;
+        System.out.println(findPages(arr,k));
     }
 }
