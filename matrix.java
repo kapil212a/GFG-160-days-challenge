46ae055223f819e98e02ace88e9887728489a5cc
kapil212a <amangupta74086@gmail.com>
2025-07-11 17:26:41 +0530

    change

--- matrix.java
+++ matrix.java
@@ -131,6 +131,61 @@ public class matrix {
         //     }
         //     return false;
     }
+
+    public static void setMatrixZeroes(int[][] mat) {
+        
+        int n = mat.length;
+        int m = mat[0].length;
+        
+        boolean firstrow = false;
+        boolean firstcol = false;
+        
+        for(int i=0 ; i<n;i++){
+            if(mat[i][0] == 0){
+               firstcol = true ;
+               break;
+            }
+        }
+        for(int j=0;j<m;j++){
+            if(mat[0][j] == 0){
+               firstrow = true;
+               break;
+            }
+        }
+        for(int i = 1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(mat[i][j]==0){
+                    mat[i][0]=0;
+                    mat[0][j] =0;
+                }
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(mat[i][0] ==0 || mat[0][j]==0){
+                    mat[i][j] = 0;
+                }
+            }
+        }
+        if(firstcol){
+            for(int i=0;i<n;i++){
+                mat[i][0] =0;
+            }
+        }
+        if(firstrow){
+            for(int j=0;j<m;j++){
+                mat[0][j] =0;
+            }
+        }
+    }
+    public static void printMat(int mat[][]){
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                System.out.print(mat[i][j] +" ");
+            }
+            System.out.println();
+        }
+    }
     
     public static void main(String[] args) {
         // int[][] mat = {
@@ -173,11 +228,19 @@ public class matrix {
         //                 int x = 9;
         // System.out.println(searchRowMatrix(mat,x));
                     
-        int mat[][] = {{1, 5, 9}, 
-                       {14, 20, 21}, 
-                       {30, 34, 43}};
-                       int x = 14;
-                       System.out.println(searchMatrix(mat, x));
+        // int mat[][] = {{1, 5, 9}, 
+        //                {14, 20, 21}, 
+        //                {30, 34, 43}
+        //             };
+        // int x = 14;
+        // System.out.println(searchMatrix(mat, x));
+
+        int mat[][] = {{0, 1, 2, 0},  
+                        {3, 4, 5, 2},  
+                        {1, 3, 1, 5}
+                    };
+        setMatrixZeroes(mat);
+        printMat(mat);
 
     }
     
