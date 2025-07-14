ae4c2899e82521cd17829c41a2c701e61ecc30bf
kapil212a <amangupta74086@gmail.com>
2025-07-14 22:33:02 +0530

    change

--- gfgarray.java
+++ gfgarray.java
@@ -39,10 +39,10 @@ public class gfgarray {
     }
 
     public static int getSecondLargest(int[] arr) {
-        // code here
+       
         int largest = 0;
         int secondlarge = 0;
-        //Arrays.sort(arr);
+        
         for (int i=0;i<arr.length;i++){
             if(arr[i]>largest){
                 largest = arr[i];
