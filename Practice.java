380d11d7ddc4ea3cd8751d7719449d4af5defac5
kapil212a <amangupta74086@gmail.com>
2025-08-04 09:30:36 +0530

    change

--- Practice.java
+++ Practice.java
@@ -1,6 +1,12 @@
 public class Practice {
+    public static int fact(int n){
+        if(n==1){
+            return 1;
+        }
+        int a = n*fact(n-1);
+        return a;
+    }
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("haiiii");
+        System.out.println(fact(5));
     }
 }
