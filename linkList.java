ca2e8bfcb4fb0d9e0deee5b8906971fb2b9b6d71
kapil212a <amangupta74086@gmail.com>
2025-09-10 19:41:50 +0530

    change

--- linkList.java
+++ linkList.java
@@ -44,6 +44,29 @@ public  class linkList {
         return head;
     }
 
+     public static Node sortedMerge(Node head1, Node head2) {
+        
+        Node dummy = new Node(-1);
+        Node curr = dummy;
+        
+        while(head1 != null && head2 != null){
+            if(head1.data <= head2.data){
+                curr.next = head1;
+                head1 = head1.next;
+            }else{
+                curr.next = head2;
+                head2 = head2.next;
+            }
+            curr = curr.next;
+        }
+        if(head1 != null){
+            curr.next = head1;
+        }else{
+            curr.next = head2;
+        }
+        return dummy.next;
+    }
+
     public static void printll(Node node){  
         while(node != null){
             System.out.print(node.data + "-->");
@@ -63,6 +86,18 @@ public  class linkList {
 
         head = rotate(head, 04);
         printll(head);
+
+        Node head1 = new Node(5);
+        head1.next = new Node(10);
+        head1.next.next = new Node(15);
+        head1.next.next.next = new Node(40);
+
+        Node head2 = new Node(2);
+        head2.next = new Node(3);
+        head2.next.next = new Node(20);
+
+        Node res = sortedMerge(head1, head2);
+        printll(res);
 
     }
 }
