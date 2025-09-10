97ba695da6b22a608077fc9c12b45efeccc625ed
kapil212a <amangupta74086@gmail.com>
2025-09-10 18:54:42 +0530

    change

--- linkList.java
+++ linkList.java
@@ -26,12 +26,31 @@ public  class linkList {
         return prev;
     }
 
+    public static Node rotate(Node head, int k) {
+        
+        if(k==0 ||  head == null){
+            return head;
+        }
+        Node temp = head;
+        for(int i = 0; i<k; ++i){
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = head;
+            temp = temp.next;
+            head = head.next;
+            temp.next = null;
+        }
+        return head;
+    }
+
     public static void printll(Node node){  
         while(node != null){
             System.out.print(node.data + "-->");
             node = node.next;
+            
         }
-        
+        System.out.println();
     }
     public static void main(String[] args) {
         Node head = new Node(1);
@@ -40,6 +59,9 @@ public  class linkList {
         head.next.next.next = new Node(4);
         head.next.next.next.next = new Node(5);
         head = reverseList(head);
+        printll(head);
+
+        head = rotate(head, 04);
         printll(head);
 
     }
