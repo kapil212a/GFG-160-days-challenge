c0ee2b39a7c8edf1905b7dc19c598d61e53add8a
kapil212a <amangupta74086@gmail.com>
2025-09-16 18:38:50 +0530

    change

--- MergeShort.java
+++ MergeShort.java
@@ -1,4 +1,4 @@
-public class MergeShort {
+public class MergeShort {                                 //complexity will (nlogn)
     public static void printArr(int arr[]){
         for(int i =0; i<arr.length;i++){
             System.out.print(arr[i] +" ");
