145ca07eadfb15197928ed417a71dd4bfb0ae7ed
kapil212a <amangupta74086@gmail.com>
2025-10-07 18:47:37 +0530

    change

--- Recursion.java
+++ Recursion.java
@@ -54,6 +54,55 @@ public class Recursion {
         
     }
 
+    public static boolean isSafe(int mat[][] , int row ,int col, int num){
+        for(int i = 0 ; i<9 ; i++){
+            if(mat[row][i] == num){
+                return false;
+            }
+        }
+        for(int i = 0 ; i<9 ; i++){
+            if(mat[i][col] == num){
+                return false;
+            }
+        }
+        
+        int startrow = row - (row % 3) , startcol = col - (col % 3);
+        for(int i = 0 ; i < 3; i++){
+            for(int j = 0; j<3 ; j++){
+                if(mat[i + startrow][j+startcol] == num){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static boolean solveSudokuOn(int mat[][] , int row , int col){
+        if(row == 8 && col == 9){
+            return true;
+        }
+        if(col == 9){
+            row++;
+            col = 0;
+        }
+        if(mat[row][col] != 0){
+            return solveSudokuOn(mat , row ,col+1);
+        }
+        for(int num = 1; num <= 9; num++){
+            if(isSafe(mat , row , col, num)){
+                mat[row][col] = num;
+                if(solveSudokuOn(mat , row , col+1)){
+                    return true;
+                }
+                mat[row][col] = 0;
+            }
+        }
+        return false;
+    }
+    public static void solveSudoku(int[][] mat) {
+        
+        solveSudokuOn(mat ,0,0);
+    }
+
 
     public static void main(String[] args) {
         double b = 3;
@@ -69,5 +118,26 @@ public class Recursion {
             }
         }
         nQueen(Board , 0);
+
+         int[][] mat = {
+            {3, 0, 6, 5, 0, 8, 4, 0, 0},
+            {5, 2, 0, 0, 0, 0, 0, 0, 0},
+            {0, 8, 7, 0, 0, 0, 0, 3, 1},
+            {0, 0, 3, 0, 1, 0, 0, 8, 0},
+            {9, 0, 0, 8, 6, 3, 0, 0, 5},
+            {0, 5, 0, 0, 9, 0, 6, 0, 0},
+            {1, 3, 0, 0, 0, 0, 2, 5, 0},
+            {0, 0, 0, 0, 0, 0, 0, 7, 4},
+            {0, 0, 5, 2, 0, 6, 3, 0, 0}
+        };
+
+        solveSudoku(mat);
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++)
+                System.out.print(mat[i][j] + " ");
+            System.out.println();
+        }
     }
+    
 }
