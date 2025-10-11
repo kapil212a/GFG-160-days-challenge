1d6aebf16707d4be912e640c7175da715c25d92d
kapil212a <amangupta74086@gmail.com>
2025-10-11 17:43:23 +0530

    change

--- tree/tree.java
+++ tree/tree.java
@@ -21,13 +21,35 @@ public class tree {
         return Math.max(lheight , rheight)+1;
     }
 
+    public static int diamter(Node root){
+        if(root == null){
+            return 0;
+        }
+        int lheight = height(root.left);
+        int rhight = height(root.right);
+
+        int ldiam = diamter(root.left);
+        int rdiam = diamter(root.right);
+
+        return Math.max(lheight+rhight , Math.max(ldiam , rdiam));
+
+    }
+
 
     public static void main (String args[]){
-        Node root = new Node(12);
-        root.right = new Node(8);
-        root.left = new Node(18);
-        root.left.right  = new Node(5);
-        root.left.left = new Node(11);
+        Node root = new Node(12);                                          //    12
+        root.right = new Node(8);                                         //   /    \
+        root.left = new Node(18);                                        //   18     8
+        root.left.right  = new Node(5);                                 //   /  \      
+        root.left.left = new Node(11);                                 //   11   5    
         System.out.println(height(root));
+
+        Node root1 = new Node(5);                                         //        5
+        root1.left = new Node(8);                                        //       /   \
+        root1.right = new Node(6);                                      //       8     6
+        root1.left.left = new Node(3);                                 //       / \   /
+        root1.left.right = new Node(7);                               //       3   7 9
+        root1.right.left = new Node(9);
+        System.out.println(diamter(root1));
     }
 }
